b2c26d4fb2b9446ab7c158459d7bebb309fd0636
Dima Gorodov <65045809+ScorpioXKiller@users.noreply.github.com>
2022-08-27 18:03:00 +0300

    Update articles.routes.js
    deleted validation parameters.

--- routes/articles.routes.js
+++ routes/articles.routes.js
@@ -20,10 +20,11 @@ articlesRouter.post(
 
     body: Joi.object()
       .keys({
-        keyword: Joi.string().required().min(1).max(10),
-        title: Joi.string().required().min(2).max(30),
-        text: Joi.string().required().min(2).max(200),
-        source: Joi.string().required().min(2).max(30),
+        keyword: Joi.string().required(),
+        title: Joi.string().required(),
+        text: Joi.string().required(),
+        date: Joi.string().required(),
+        source: Joi.string().required(),
         link: Joi.string().required().custom(validateUrl),
         image: Joi.string().required().custom(validateUrl),
         user: Joi.object()
